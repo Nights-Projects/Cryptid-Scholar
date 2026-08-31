1ef6a4635a28f012747c23ea35215366b79386db
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2026-08-31 16:06:57 -0500

    fix: remove Compose plugin, fix XML attributes, increase JVM heap
    Fix all APK build issues: remove Compose plugin, fix XML attributes to CamelCase, remove JVM GC flag, increase heap.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 android {
@@ -36,10 +35,6 @@ android {
     buildFeatures {
         viewBinding = true
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
