c9909dc0a8b393f7ce4431acd658984dd6cfca58
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2026-08-31 14:52:13 -0500

    Fix android/build.gradle.kts: Kotlin DSL plugin version + syntax

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,11 @@
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
 buildscript {
-    val composeVersion = "2024.10.00"
     val kotlinVersion = "2.1.0"
+    val composeVersion = "2024.10.00"
     repositories {
         google()
         mavenCentral()
@@ -8,5 +13,12 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("androidx.compose:compose-bom:$composeVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
